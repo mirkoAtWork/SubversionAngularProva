87be5ec8181cdbee72de4c63749534e31f7c7c2d
mirkoAtWork <mirko.medone@gmail.com>
2017-02-22 16:53:46 +0100

    Ng-Repeat

--- control.js
+++ control.js
@@ -1,12 +1,11 @@
-angular.module("AppRipassoDelMartedi",[])
-  .controller("anagrafica",function($scope){
-  this.nome="mirko";
-  $scope.salta="salta";
-
-  })
-  .controller("saluti",function(){
-    this.saluta=function(anagr){
-      return "Ciao a te, utente"+" "+ (anagr.nome || "Ospite");
-      }
-
-  });
+angular.module("mercolediApp",[])
+.controller("tabella",function(){
+  this.elenco=["primo","secondo","secondo","terzo"];
+  this.elencodup=[53,27,27,27,32,53,1];
+  this.rubrica=[
+              {nome:"mirko",indirizzo:"via le dita dal naso",tel:"60610106"},
+              {nome:"mirko",indirizzo:"via le dita dal naso",tel:"60610106"},
+              {nome:"Davide",indirizzo:"casa sua",tel:"123456"},
+              {nome:"un nome",indirizzo:"un indirizzo",tel:"un numero di telefono"}
+            ];
+});
