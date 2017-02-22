e0101f9b76333519b0f9528df80976370fbd3c9d
mirkoAtWork <mirko.medone@gmail.com>
2017-02-22 14:43:39 +0100

    Da Non Fare! Mischiare $scope e controller as? Fatto!

--- control.js
+++ control.js
@@ -1,9 +1,12 @@
 angular.module("AppRipassoDelMartedi",[])
-  .controller("anagrafica",function(){
+  .controller("anagrafica",function($scope){
   this.nome="mirko";
+  $scope.salta="salta";
+
   })
   .controller("saluti",function(){
     this.saluta=function(anagr){
       return "Ciao a te, utente"+" "+ (anagr.nome || "Ospite");
       }
+
   });
